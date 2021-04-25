bedde8883966de9d65813b18e1a25ccf9d9f526c
Liam So <Liam.So@dal.ca>
2021-04-25 17:05:46 -0300

    Added navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,10 @@
-import { Link, useHistory } from 'react-router-dom'
-import { auth } from '../config/firebase';
+import { Navbar } from '../components/Navbar';
 
 const Home = () => {
 
-    const history = useHistory();
-
-    const Logout = () => {
-        auth.signOut()
-        .then(() => history.push('/login'))
-        .catch(error => console.log(error));
-    }
-
     return (
         <div>
-            <div>
-                Welcome to the home page
-            </div>
-            <button className="py-2 px-2 rounded-full bg-red-200 font-sans">
-                <Link to='/change'>
-                    Change your password
-                </Link>
-            </button>
-            <button className="py-2 px-2 rounded-full bg-indigo-200 font sans" onClick={() => Logout()}>
-                Log out
-            </button>
+            <Navbar/>
         </div>
     )
 }
