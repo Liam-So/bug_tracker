1f1b80a555721b45fa65df5d6c676a90702b6838
Liam So <Liam.So@dal.ca>
2021-04-25 15:40:42 -0300

    Added protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, RouteProps, RouteComponentProps } from "react-router-dom"
 import routes from "./config/routes"
 import AuthRoute from "./components/AuthRoute"
-import { auth } from "./config/firebase";
-import { useState } from "react";
 
 function App() {
 
@@ -11,7 +9,21 @@ function App() {
       <Router>
         <Switch>
           {routes.map((route, index) => (
-            <Route key={index} path={route.path} exact={route.exact} component={route.component}/>
+            <Route 
+              key={index} 
+              path={route.path}  
+              exact={route.exact} 
+              render={(routeProps: RouteComponentProps<any>) => {
+                if (route.protected)
+                return (
+                  <AuthRoute>
+                    <route.component {...routeProps} />
+                  </AuthRoute>
+                );
+
+              return <route.component {...routeProps} />;
+              }}
+            />
           ))}
         </Switch>
       </Router>
