8e87eefea2b73e3ddb35cc1f14eb6f6775e84991
Liam So <Liam.So@dal.ca>
2021-04-25 22:16:20 -0300

    Created new collection to store new users

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react';
 import HomeTable from '../components/HomeTable/HomeTable';
 import { Navbar } from '../components/Navbar';
+import Firebase from '../config/firebase';
 
 const Home = () => {
+
+    const [user, setUser] = useState([]);
+
+    const ref = Firebase.firestore().collection('users');
+
+    console.log(ref.doc('VWtHHhdkbiYsNWAWkfOl2SieicJ3').get())
+
+    function getSchools() {
+        ref.onSnapshot((querySnapshot) => {
+            const items:any = [];
+
+            querySnapshot.forEach(doc => {
+                items.push(doc.data());
+            })
+
+            setUser(items);
+        })
+    }
+
+    useEffect(() => {
+        getSchools();
+    }, [])
 
     return (
         <div>
             <Navbar/>
             <HomeTable/>
+            {console.log(user)}
         </div>
     )
 }
